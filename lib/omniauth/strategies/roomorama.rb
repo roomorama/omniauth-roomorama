5b514e80160bafb7bb45efd4c7078b2337f76995
Donald Piret <donald@donaldpiret.com>
2013-03-03 21:52:47 +0800

    Add support for basic information

--- lib/omniauth/strategies/roomorama.rb
+++ lib/omniauth/strategies/roomorama.rb
@@ -6,10 +6,35 @@ module OmniAuth
       option :name, 'roomorama'
 
       option :client_options, { 
-        :site => 'https://roomorama.com', 
-        :authorize_url => "/oauth/authorize",
-        :token_url => '/oauth/token'
+        :site => 'https://api.roomorama.com', 
+        :authorize_url => "https://roomorama.com/oauth/authorize",
+        :token_url => "https://roomorama.com/oauth/token"
       }
+      
+      uid { raw_info['id'] }
+      
+      info do
+        {
+          name: raw_info['login'],
+          email: raw_info['email'],
+          nickname: raw_info['login'],
+          first_name: raw_info['first_name'],
+          last_name: raw_info['last_name'],
+          location: [raw_info['city'], raw_info['country']].delete_if(&:blank?).join(', '),
+          description: raw_info['description'],
+          phone: raw_info['phone_number'],
+        }
+      end
+      
+      extra do
+        {
+          :raw_info => raw_info
+        }
+      end
+      
+      def raw_info
+        @raw_info ||= access_token.get('/v1.0/me').parsed['result']
+      end
     end
   end
 end
